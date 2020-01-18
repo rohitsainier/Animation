7e5de17c60023105e779a5769d22d62e01ae1bfa
Rohit Saini <sketchlearn7@gmail.com>
2020-01-19 00:30:28 +0530

    Some modification added in animation

--- animation/ViewController.swift
+++ animation/ViewController.swift
@@ -12,10 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let curved  = curvedView(frame: view.frame)
+//        let curved  = curvedView(frame: CGRect(x: 0, y: 100, width: view.frame.width, height: view.frame.height - 100))
 //        curved.backgroundColor = UIColor.yellow
 //        view.addSubview(curved)//CURVED
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+         
+          view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
         
     }
     
@@ -40,13 +41,63 @@ class ViewController: UIViewController {
         view.addSubview(imageView)//IMAGEVIEW
     }
     
+    //MARK:- sendEmoji
+    func sendEmoji(emojiIcon: UIImage){
+        let imageView = UIImageView()
+        let dimention = 40 + drand48() * 10
+        imageView.frame = CGRect(x: 0, y: 0, width: dimention, height: dimention)
+        imageView.image = emojiIcon
+        let animation = CAKeyframeAnimation(keyPath: "position")
+        animation.path = CustomPath().cgPath
+        animation.duration = 2 + drand48() * 3
+        animation.fillMode = kCAFillModeForwards
+        animation.isRemovedOnCompletion = false
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        imageView.layer.add(animation, forKey: nil)
+        view.addSubview(imageView)
+    }
+    
+    @IBAction func clickBtn1(_ sender: UIButton) {
+        (0...10).forEach { (_) in
+            sendEmoji(emojiIcon: #imageLiteral(resourceName: "chat-bubble"))
+        }
+    }
+    
+    @IBAction func clickBtn2(_ sender: UIButton) {
+        (0...10).forEach { (_) in
+            sendEmoji(emojiIcon: #imageLiteral(resourceName: "heart"))
+        }
+    }
+    @IBAction func clickBtn3(_ sender: UIButton) {
+        (0...10).forEach { (_) in
+            sendEmoji(emojiIcon: #imageLiteral(resourceName: "connector"))
+        }
+    }
+    
+    @IBAction func clickBtn4(_ sender: UIButton) {
+        (0...10).forEach { (_) in
+            sendEmoji(emojiIcon: #imageLiteral(resourceName: "loving"))
+        }
+    }
+    @IBAction func clickBtn5(_ sender: UIButton) {
+        (0...10).forEach { (_) in
+            sendEmoji(emojiIcon: #imageLiteral(resourceName: "favourite"))
+        }
+    }
+    @IBAction func clickBtn6(_ sender: UIButton) {
+        (0...10).forEach { (_) in
+            sendEmoji(emojiIcon: #imageLiteral(resourceName: "india"))
+        }
+    }
+    
 }
     
     func CustomPath() -> UIBezierPath{
         let  path = UIBezierPath()
-        path.move(to: CGPoint(x: 0, y: 400))
-        let endPoint = CGPoint(x: 400, y:200)
-        let random = 200 + drand48() * 300
+        path.move(to: CGPoint(x: 0, y: 0))
+        let random = 0 + drand48() * 600
+        let endPoint = CGPoint(x: random, y:1000)
+        
         let cp1 = CGPoint(x: 300, y: 100 - random)
         let cp2 = CGPoint(x: 100, y: 200 + random)
         path.addCurve(to: endPoint, controlPoint1: cp1, controlPoint2: cp2)
